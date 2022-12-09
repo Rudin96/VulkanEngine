7d30d363aae5c06b6f4f2372534505569460cacc
Tobias Rudin <tobias_rudin@hotmail.se>
2022-12-09 08:36:29 +0100

    started adding some input

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -778,9 +778,11 @@ void Engine::updateUniformBuffer(uint32_t currentImage)
 
     auto currentTime = std::chrono::high_resolution_clock::now();
     float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+    float rotSpeed = Input::getInstance().IsLeftMousePressed() ? 1.f : 5.f;
+    float rotAmount = glm::radians(90.f) * time * rotSpeed;
 
     UniformBufferObject ubo{};
-    ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	ubo.model = glm::rotate(glm::mat4(1.0f), rotAmount, glm::vec3(0.0f, 1.0f, 1.0f));
     ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
 
@@ -1523,8 +1525,9 @@ bool Engine::checkValidationLayerSupport() {
 
 void Engine::initInput()
 {
-    glfwSetCursorPosCallback(window, Input::cursor_callback);
-    glfwSetKeyCallback(window, Input::keyboardKey_callback);
+    glfwSetCursorPosCallback(window, Input::getInstance().cursor_callback);
+    glfwSetKeyCallback(window, Input::getInstance().keyboardKey_callback);
+    glfwSetMouseButtonCallback(window, Input::getInstance().mouse_button_callback);
 }
 
 void Engine::createSurface()
--- Engine/Input/Input.cpp
+++ Engine/Input/Input.cpp
@@ -1,11 +1,6 @@
 #include "Input.h"
 #include <GLFW/glfw3.h>
 
-Input* Input::GetInstance()
-{
-	return nullptr;
-}
-
 Input::Input()
 {
 
@@ -13,7 +8,7 @@ Input::Input()
 
 void Input::cursor_callback(GLFWwindow* window, double xPos, double yPos)
 {
-	
+	getInstance().mousePos = Vector2(xPos, yPos);
 }
 
 void Input::keyboardKey_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -23,7 +18,20 @@ void Input::keyboardKey_callback(GLFWwindow* window, int key, int scancode, int 
 	//Logger::Log(action);
 }
 
+void Input::mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+		getInstance().leftMouseButtonPressed = true;
+	else
+		getInstance().leftMouseButtonPressed = false;
+}
+
 Vector2 Input::GetMousePosition()
 {
 	return Vector2(mousePos);
+}
+
+bool Input::IsLeftMousePressed()
+{
+	return leftMouseButtonPressed;
 }

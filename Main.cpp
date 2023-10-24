41a82bed18ffc60f1e543a2404315eebd2d33913
Tobias Rudin <tobias_rudin@hotmail.se>
2023-10-24 21:29:52 +0200

    .

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,3 @@
-#include "Engine/Engine.h"
-#include "Utils/Logger.h"
-
 int main() {
-    Engine engine;
-
-    try {
-        engine.run();
-    }
-    catch (const std::exception& e) {
-        Logger::LogError(e.what());
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+    return 0;
 }
